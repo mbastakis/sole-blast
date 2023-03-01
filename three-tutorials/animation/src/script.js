0b9e9a990642aca8989ad950a84fb474cc247c4f
Tehmike <mbastakis@gmail.com>
2023-03-01 19:18:16 +0200

    Finish Animation

--- three-tutorials/animation/src/script.js
+++ three-tutorials/animation/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -28,4 +29,36 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+// Animations
+// Simple Time
+// let time = Date.now();
+
+// Threejs clock
+// const clock = new THREE.Clock();
+
+// External gsap library
+// gsap.to(mesh.position, { x: 2, duration: 2, delay: 2 });
+// gsap.to(mesh.position, { x: 0, duration: 2, delay: 4 });
+
+const tick = () => {
+    // Simple Time 
+    // const currentTime = Date.now();
+    // const deltaTime = currentTime - time;
+    // time = currentTime;
+    
+    // Threejs clock
+    // const elapsedTime = clock.getElapsedTime();
+    // mesh.position.x = Math.cos(elapsedTime);
+    // mesh.position.y = Math.sin(elapsedTime);
+    // mesh.rotation.y = elapsedTime * Math.PI / 2;
+    // mesh.rotation.z = elapsedTime;
+    
+    
+    
+
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick);
+}
+
+tick();
