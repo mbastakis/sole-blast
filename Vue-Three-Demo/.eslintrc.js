f5ca85038ef0249b8c827445389f1949d8252ef9
Tehmike <mbastakis@gmail.com>
2023-03-30 15:27:57 +0300

    Added lint and prettier

--- Vue-Three-Demo/.eslintrc.js
+++ Vue-Three-Demo/.eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/essential",
+    "@vue/prettier",
+    "prettier",
+  ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
