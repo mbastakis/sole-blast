b5fd2a84439459f1e4a9ada3d2afa31bb918698e
mbastakis <mbastakis@gmail.com>
2023-06-03 19:31:28 +0300

    testing multilanguage

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,9 +1,23 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
+import messages from "@intlify/unplugin-vue-i18n/messages";
+
+console.log(navigator.language);
+
+const i18n = createI18n({
+    legacy: false,
+    locale: 'en',
+    globalInjection: true,
+    fallbackLocale: 'en',
+    availableLocales: ['en', 'gr'],
+    messages: messages,
+})
 
 const app = createApp(App)
 
+app.use(i18n)
 app.use(router)
 
 app.mount('#app')
