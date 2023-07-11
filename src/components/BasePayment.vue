62b3b846fd930fb33ca224d623c8e5ac9109d46f
mbastakis <mbastakis@gmail.com>
2023-07-11 19:28:09 +0300

    added payment component

---
+++ src/components/BasePayment.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <iframe
+      ref="iframe"
+      name="my-hosted-form"
+      :data-sc-key="scKey"
+      :data-name="name"
+      :data-description="description"
+      :data-reference="reference"
+      :data-amount="amount"
+      :data-color="color"
+      style="border: none; height: 500px; overflow: hidden"
+    >
+    </iframe>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const scKey = 'sbpb_YzYxOWMwNTItYTRlMi00MTM4LTllZmMtYTI3NzFkYjdjMDky'
+const name = 'Jasmine Green Tea'
+const description = 'Smooth tea with a rich jasmine bouquet'
+const reference = '99999'
+const amount = '10'
+const color = '#282e5c'
+
+let iframe = ref(null)
+
+onMounted(() => {
+  let script = document.createElement('script')
+  script.type = 'text/javascript'
+  script.src = 'https://www.simplify.com/commerce/simplify.pay.js'
+  document.body.appendChild(script)
+})
+</script>
+
+<style scoped>
+iframe {
+  border: none;
+  height: 500px;
+  overflow: hidden;
+}
+</style>
--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,31 +1,16 @@
 <template>
   <div id="app">
-    <iframe id="paymentFrame"></iframe>
-    <button @click="makePayment">Make Payment</button>
+    <BasePayment />
   </div>
 </template>
 
 <script>
+import BasePayment from '../components/BasePayment.vue'
+
 export default {
   name: 'TestView',
-  methods: {
-    makePayment() {
-      var paymentFrame = document.getElementById('paymentFrame')
-
-      window.SimplifyCommerce.hostedPayments(this.paymentResponseHandler, {
-        operation: 'create.payment',
-        publicKey: 'YOUR_HOSTED_PAYMENTS_ENABLED_PUBLIC_KEY',
-        frameId: 'paymentFrame'
-      })
-    },
-    paymentResponseHandler(response) {
-      // Handle the payment response here
-      if (response.paymentStatus === 'APPROVED') {
-        console.log('Payment successful')
-      } else {
-        console.log('Payment failed')
-      }
-    }
+  components: {
+    BasePayment
   }
 }
 </script>
